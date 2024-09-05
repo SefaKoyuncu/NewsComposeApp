5ebdd552847f07572d0b5069ba67e8479f6f1b95
SefaKoyuncu <sefakoyuncu1@outlook.com>
2024-09-05 04:04:01 +0300

    initialized and font added, theme changed

---
+++ app/src/main/java/com/sefa/newsapp/ui/theme/RobotoFontFamily.kt
@@ -0,0 +1,18 @@
+package com.sefa.newsapp.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.sefa.newsapp.R
+
+// Roboto Font Family
+val RobotoFontFamily = FontFamily(
+    Font(R.font.robotoregular, FontWeight.Normal),  // Normal
+    Font(R.font.robotobold, FontWeight.Bold),  // Bold
+    Font(R.font.robotoblack, FontWeight.Black),  // Black
+    Font(R.font.robotolight, FontWeight.Light),  // Light
+    Font(R.font.robotoextrabold, FontWeight.ExtraBold),  // ExtraBold
+    Font(R.font.robotomedium, FontWeight.Medium),  // Medium
+    Font(R.font.robotosemibold, FontWeight.SemiBold),  // SemiBold
+    Font(R.font.robotothin, FontWeight.Thin),  // Thin
+)
---
+++ app/src/main/java/com/sefa/newsapp/ui/theme/Type.kt
@@ -0,0 +1,86 @@
+package com.sefa.newsapp.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 57.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 45.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 36.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 28.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = RobotoFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp
+    )
+)
