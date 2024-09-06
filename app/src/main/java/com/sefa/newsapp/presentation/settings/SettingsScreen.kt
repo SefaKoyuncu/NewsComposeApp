bf031301ca69f4699bfd01d4209b644e38e33ab6
SefaKoyuncu <sefakoyuncu1@outlook.com>
2024-09-06 23:56:17 +0300

    added info dialog and page title on setting screen

--- app/src/main/java/com/sefa/newsapp/presentation/settings/SettingsScreen.kt
+++ app/src/main/java/com/sefa/newsapp/presentation/settings/SettingsScreen.kt
@@ -4,6 +4,10 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -12,9 +16,15 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.sefa.newsapp.presentation.theme.blue
+import com.sefa.newsapp.presentation.theme.lightBlue
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 @Composable
 fun SettingsScreen() {
+
+    var showDialog by remember { mutableStateOf(false) } // Dialog'u göster/gizle durumu
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -22,17 +32,20 @@ fun SettingsScreen() {
         verticalArrangement = Arrangement.Top, // Elemanları üste hizala
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        // Üstteki "Settings" texti
+
+        // Başlık bölümü
         Text(
             text = "Settings",
-            style = MaterialTheme.typography.headlineMedium,
-            modifier = Modifier.padding(top = 16.dp),
-            fontSize = 28.sp,
-            fontWeight = FontWeight.SemiBold,
-            color = Color.Black
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(bottom = 8.dp)
+                .padding(top = 8.dp)
+                .align(Alignment.CenterHorizontally),
+            style = MaterialTheme.typography.displayMedium
         )
 
-        Spacer(modifier = Modifier.height(100.dp)) // "Settings" ile buton arasına boşluk ekle
+        Spacer(modifier = Modifier.weight(1f)) // "Settings" ile buton arasına boşluk ekle
 
         // Ortada "Log out" butonu
         Button(
@@ -45,9 +58,10 @@ fun SettingsScreen() {
             Text(text = "Log out", color = Color.White)
         }
 
-        // Buton ve ClickableText arasındaki boşluğu minimuma indirdik
         TextButton(
-            onClick = {}, // Navigate to SignUpScreen
+            onClick = {
+                showDialog = true
+            },
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
                 .padding(top = 13.dp)
@@ -60,16 +74,56 @@ fun SettingsScreen() {
 
         Spacer(modifier = Modifier.weight(1f)) // Ekranın geri kalanını boş bırak
 
-        // En altta ortalanmış text
+        // NYTNews
         Text(
             text = "NYTNews",
-            fontSize = 14.sp,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.ExtraBold,
+            style = MaterialTheme.typography.displayMedium,
             textAlign = TextAlign.Center,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(bottom = 16.dp),
-            style = MaterialTheme.typography.labelLarge,
-            fontWeight = FontWeight.ExtraBold,
+        )
+    }
+
+    // AlertDialog
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            title = {
+                Text(
+                    text = "Change Password ",
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.labelMedium,
+                    fontSize = 23.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.fillMaxWidth(),
+                    color = Color.Black
+                )
+
+                    },
+            text = {
+                Text(
+                    text = "A link to change your password has been sent to your email address.",
+                    color = Color.DarkGray,
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.labelMedium,
+                    fontSize = 13.sp,
+                    fontWeight = FontWeight.Medium,
+                    ) },
+            confirmButton = {
+                TextButton(onClick = { showDialog = false }) {
+                    Text(text = "OK",
+                        color = Color.Black,
+                        style = MaterialTheme.typography.labelMedium,
+                        fontSize = 13.sp,
+                        fontWeight = FontWeight.ExtraBold,
+                        )
+                }
+            },
+            containerColor = Color.LightGray, // AlertDialog arka plan rengi
         )
     }
 }
