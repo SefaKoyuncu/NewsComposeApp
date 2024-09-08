6ee524a2a0aa329b84be807d6dbc5c103d0d3917
SefaKoyuncu <sefakoyuncu1@outlook.com>
2024-09-08 14:44:42 +0300

    data model optimized and domain model added, mapper extensions added

--- app/src/main/java/com/sefa/newsapp/data/model/NewsResponse.kt
+++ app/src/main/java/com/sefa/newsapp/data/model/NewsResponse.kt
@@ -2,11 +2,10 @@ package com.sefa.newsapp.data.model
 
 import com.google.gson.annotations.SerializedName
 
-
 data class NewsResponse (
 
   @SerializedName("status"      ) var status     : String?            = null,
   @SerializedName("copyright"   ) var copyright  : String?            = null,
   @SerializedName("num_results" ) var numResults : Int?               = null,
-  @SerializedName("results"     ) var news    : List<News> = listOf()
+  @SerializedName("results"     ) var news       : List<News>         = listOf()
 )
---
+++ app/src/main/java/com/sefa/newsapp/domain/model/NewsUIModel.kt
@@ -0,0 +1,25 @@
+package com.sefa.newsapp.domain.model
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.Serializable
+
+@Entity(tableName = "news")
+@Serializable
+@Parcelize
+data class NewsUIModel (
+
+  var url           : String?           = null,
+  @PrimaryKey //autoGenerate = true
+  var id            : Long?              = null,
+  var publishedDate : String?           = null,
+  var section       : String?           = null,
+  var subsection    : String?           = null,
+  var byline        : String?           = null,
+  var title         : String?           = null,
+  var abstract      : String?           = null,
+  var imageUrl      : String?           = null
+) : Parcelable
