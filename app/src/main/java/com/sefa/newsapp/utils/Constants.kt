665dae86068c739b1f936ac1c876d4101933ecfb
SefaKoyuncu <sefakoyuncu1@outlook.com>
2024-09-08 15:07:18 +0300

    constants, date formatter extensions and resource sealed class added

---
+++ app/src/main/java/com/sefa/newsapp/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.sefa.newsapp.utils
+
+object Constants
+{
+    const val BASE_URL = "https://api.nytimes.com/svc/mostpopular/v2/"
+    const val API_KEY = "AyyZAeVEHMQlIYnv40Q4f1OJW7C0UyuG"
+    const val END_POINT = "viewed/30.json"
+    const val NO_IMAGE = "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
+}
