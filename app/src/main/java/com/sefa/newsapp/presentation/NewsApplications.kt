92b0d8c96c1302d20eb8664ff50a21af2c49722d
SefaKoyuncu <sefakoyuncu1@outlook.com>
2024-09-10 18:10:52 +0300

    added firebase crashlytics and leakcanary

--- app/src/main/java/com/sefa/newsapp/presentation/NewsApplications.kt
+++ app/src/main/java/com/sefa/newsapp/presentation/NewsApplications.kt
@@ -1,7 +1,17 @@
 package com.sefa.newsapp.presentation
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class NewsApplications : Application()
+{
+    override fun onCreate()
+    {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+        FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(true)
+    }
+}
